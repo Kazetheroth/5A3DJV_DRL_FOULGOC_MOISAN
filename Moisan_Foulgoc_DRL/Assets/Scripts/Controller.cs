128aefc24a7329e9abe4aa6f58e241cadf35cc61
Pim <pim103@hotmail.fr>
2021-03-08 12:08:05 +0100

    Add interfaces - implement cell type

--- Moisan_Foulgoc_DRL/Assets/Scripts/Controller.cs
+++ Moisan_Foulgoc_DRL/Assets/Scripts/Controller.cs
@@ -1,6 +1,19 @@
-﻿using UnityEngine;
+﻿using Interfaces;
+using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public enum GameType
+    {
+        GridWORLDO,
+        TicTacTard,
+        Soooookolat,
+    }
     
+    private IGame game;
+
+    void StartGame(GameType gameType)
+    {
+        
+    }
 }
--- Moisan_Foulgoc_DRL/Assets/Scripts/GridWORLDO/GridWORDOGame.cs
+++ Moisan_Foulgoc_DRL/Assets/Scripts/GridWORLDO/GridWORDOGame.cs
@@ -1,12 +1,29 @@
-﻿using Interfaces;
+﻿using System.Collections.Generic;
+using Interfaces;
 
 namespace GridWORLDO
 {
     public class GridWORDOGame : IGame
     {
+        private IPlayer player;
+
+        private List<List<ICell>> cells;
+        
         public bool InitGame()
         {
-            throw new System.NotImplementedException();
+            cells = new List<List<ICell>>();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                List<ICell> cellsPerLine = new List<ICell>();
+                
+                for (int j = 0; j < 10; ++j)
+                {
+                    cellsPerLine.Add(new GridWorldCell());
+                }
+            }
+
+            return true;
         }
 
         public bool UpdateGame()
