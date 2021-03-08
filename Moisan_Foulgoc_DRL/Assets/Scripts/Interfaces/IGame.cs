4c58b160887884fbabb641d625de849c6568f9ac
Pim <pim103@hotmail.fr>
2021-03-08 11:53:35 +0100

    Init scene

--- Moisan_Foulgoc_DRL/Assets/Scripts/Interfaces/IGame.cs
+++ Moisan_Foulgoc_DRL/Assets/Scripts/Interfaces/IGame.cs
@@ -2,6 +2,8 @@
 {
     public interface IGame
     {
-        
+        bool InitGame();
+        bool UpdateGame();
+        bool EndGame();
     }
 }
--- Moisan_Foulgoc_DRL/Assets/Scripts/Soooookolat/SoooookolatGame.cs
+++ Moisan_Foulgoc_DRL/Assets/Scripts/Soooookolat/SoooookolatGame.cs
@@ -4,6 +4,19 @@ namespace Soooookolat
 {
     public class SoooookolatGame : IGame
     {
-        
+        public bool InitGame()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpdateGame()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool EndGame()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
